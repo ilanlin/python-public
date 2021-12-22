dd101fb22f7475c479c2bd40745467994af78dc9
ilanlin <710057201@qq.com>
2021-12-22 14:34:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# 请您来修正Hello, word的拼写错误吧！
-# 请在修改完成后，通过主菜单 Git/Commit ... 菜单项完成代码的Commit 和 Push。
-# Push完成后回到Factory ( http://factory.cloudtogo.cn/project/blueprint?id=last )，用同样的方法发布一个新实例即可看到修改后的效果。
-
-
-
-
-
 @app.route('/')
 def hello():
-    return "hello-world-again-again-again1010,public"
-
+    return '恭喜您成功地发布了一个运行在云端的python-public程序，公网github,main分支'
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=81)
